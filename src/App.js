2c0350969b8f396ffdd420ed9e2277c15974f502
madoka <menomonies@gmail.com>
2019-07-10 13:03:15 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Mame chan!
+          Hi Kina from the feature branch!
         </p>
         <a
           className="App-link"
